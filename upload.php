97c4fe3a92a6c7e28166020ba3351bf2fd510555
Nadim Kobeissi <nadim@nadim.cc>
2012-03-29 14:29:05 -0400

    GitHub for Mac: Throw-away commit.

---
+++ upload.php
@@ -0,0 +1,15 @@
+<?php
+if ($_FILES['file']['error'] > 0
+|| !preg_match('/^(\w|\s){5,64}$/', $_POST['tag'])
+|| !preg_match('/audio/', $_FILES['file']['type'])
+|| !preg_match('/^(mp3|aac|wav|ogg|webm)$/', pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION))) {
+	echo 'ERROR';
+}
+else if (file_exists('s/'.$_FILES['file']['name'])) {
+	echo 'EXIST';
+}
+else {
+	move_uploaded_file($_FILES['file']['tmp_name'], 's/'.$_POST['tag'].'.'.pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION));
+	echo 'OK';
+}
+?>
